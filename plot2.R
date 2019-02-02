6a129e8221baefd2f53673df9eb71cdf97518eb9
cmoldez <45146876+cmoldez@users.noreply.github.com>
2019-02-02 22:45:04 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,17 +1,17 @@
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
+nei <- readRDS("data/summarySCC_PM25.rds")
+scc <- readRDS("data/Source_Classification_Code.rds")
 
-# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
-# Use the base plotting system to make a plot answering this question.
+library('data.table')
 
-subsetNEI  <- NEI[NEI$fips=="24510", ]
+df <- data.table(nei)
 
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+baltimore <- subset(df, fips == '24510')
 
-png('plot2.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+by_year <- baltimore[, list(emissions=sum(Emissions)), by=year]
+by_year$year = as.numeric(as.character(by_year$year))
+by_year$emissions = as.numeric(as.character(by_year$emissions))
+
+plot(by_year$year, by_year$emissions, type='l', ylab='Emissions', xlab='Year')
+
+dev.copy(png, file="plot2.png", width=480, height=480)
 dev.off()
